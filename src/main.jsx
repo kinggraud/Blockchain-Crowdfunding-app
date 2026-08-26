1612b127f9a37d84eb4f3d9dacc7edb0c99a7b5c
SAMMEE . <abahsamuel2003@gmail.com>
2026-08-26 18:42:57 +0100

    Add fallback RPCs

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,15 @@ import ScrollToTop from './utils/ScrollToTop';
 import App from './App';
 import './index.css';
 
+const customSepolia = {
+  ...Sepolia,
+  rpc: [
+    "https://rpc.ankr.com/eth_sepolia",
+    "https://ethereum-sepolia-rpc.publicnode.com",
+    "https://sepolia.gateway.tenderly.co",
+  ],
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
